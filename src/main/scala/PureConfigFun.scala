a4acd2ab6673ccad5beaf7998c1516e1095f74c0
Mike Slinn <mslinn@micronauticsresearch.com>
2019-12-19 19:23:09 -0800

    -

--- src/main/scala/PureConfigFun.scala
+++ src/main/scala/PureConfigFun.scala
@@ -8,16 +8,9 @@ import pureconfig.error._
 import com.typesafe.config.ConfigValueType._
 import pureconfig.ConfigReader.Result
 
-// FIXME this works when run from IDEA, but 'sbt runMain PureConfigTest' yields this error:
-// Error: ConfigReaderFailures(CannotReadFile(file:/tmp/sbt_60a15cbd/job-1/target/2d0db0fc/7e27f317/pure-config-test_2.13-0.1.2.jar!/pure.conf,Some(java.io.FileNotFoundException: file:/tmp/sbt_60a15cbd/job-1/target/2d0db0fc/7e27f317/pure-config-test_2.12-0.1.2.jar!/pure.conf (No such file or directory))),List())
-//
-// Also, building an assembly and running it gives a similar error:
+// Build an assembly and run it:
 // $ sbt assembly
 // $ java -cp target/scala-2.13/pure-config-test-assembly-0.1.2.jar PureConfigTest
-// Error: ConfigReaderFailures(CannotReadFile(file:/mnt/c/work/experiments/scala/pure-config-test/target/scala-2.13/pure-config-test-assembly-0.1.2.jar!/pure.conf,Some(java.io.FileNotFoundException: file:/mnt/c/work/experiments/scala/pure-config-test/target/scala-2.13/pure-config-test-assembly-0.1.2.jar!/pure.conf (No such file or directory))),List())
-// This is weird because the file is in the jar:
-// $ jar -tf /mnt/c/work/experiments/scala/pure-config-test/target/scala-2.13/pure-config-test-assembly-0.1.2.jar | grep pure.conf
-//pure.conf
 object PureConfigTest extends App {
   PureConfigFun.load match {
     case Right(right) => println(s"Success: $right")
@@ -26,19 +19,9 @@ object PureConfigTest extends App {
   }
 }
 
-// FIXME this works when run from IDEA, but 'sbt runMain PureConfigTest2' yields this error:
-// Error: ConfigReaderFailures(CannotReadFile(file:/tmp/sbt_522e3bb2/job-3/target/c8244565/94a30183/pure-config-test_2.13-0.1.2.jar!/pure.conf,Some(java.io.FileNotFoundException: file:/tmp/sbt_522e3bb2/job-3/target/c8244565/94a30183/pure-config-test_2.13-0.1.2.jar!/pure.conf (No such file or directory))),List())
-// Error: pureconfig.error.ConfigReaderException: Cannot convert configuration to a PureConfigFun. Failures are:
-//  - Unable to read file file:/tmp/sbt_522e3bb2/job-1/target/2d0db0fc/49472053/pure-config-test_2.13-0.1.2.jar!/pure.conf (No such file or directory).
-//
-// Also, building an assembly and running it gives a similar error:
+// Build an assembly and run it:
 // $ sbt assembly
 // $ java -cp target/scala-2.13/pure-config-test-assembly-0.1.2.jar PureConfigTest2
-// Error: pureconfig.error.ConfigReaderException: Cannot convert configuration to a PureConfigFun. Failures are:
-//  - Unable to read file file:/mnt/c/work/experiments/scala/pure-config-test/target/scala-2.13/pure-config-test-assembly-0.1.2.jar!/pure.conf (No such file or directory).
-// This is weird because the file is in the jar:
-// $ jar -tf /mnt/c/work/experiments/scala/pure-config-test/target/scala-2.13/pure-config-test-assembly-0.1.2.jar | grep pure.conf
-//pure.conf
 object PureConfigTest2 extends App {
   try {
     val pureConfigFun: PureConfigFun = PureConfigFun.loadOrThrow
